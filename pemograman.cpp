82179ca6adf686179dad0679b6add317ee9746c8
bagasdisyah <144640778+bagasdisyah@users.noreply.github.com>
2023-09-11 09:22:30 +0700

    Create pemograman.cpp

---
+++ pemograman.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    int a,b,c ;
+    cout << "PEMROGRAMAN OPRASI ARITMATIKA\n" ;
+    cout<<"masukan angka pertama: ";
+    cin >>a;
+    cout<<"memasukan angka kedua:";
+    cin>>b;
+  
+     cout<<"Hasil penjumlahan: "<< a+b << endl;
+
+     cout<<"Hasil pengurangan: "<< a-b << endl;
+
+     cout<<"Hasil perkalian: "<< a*b << endl;
+
+     cout<<"Hasil pembagian: "<< a/b << endl;
+
+     cout<<"Hasil modulus: "<< a%b << endl;
+
+return 0;
+}
